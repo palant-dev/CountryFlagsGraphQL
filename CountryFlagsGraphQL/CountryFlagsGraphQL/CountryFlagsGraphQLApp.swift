5085fede76967f5daf24b43f887248bd88a22f98
palant-dev <antonio.palomba2015@gmail.com>
2024-07-04 10:50:31 +0200

    First commit with project creation

---
+++ CountryFlagsGraphQL/CountryFlagsGraphQL/CountryFlagsGraphQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryFlagsGraphQLApp.swift
+//  CountryFlagsGraphQL
+//
+//  Created by Antonio Palomba on 04/07/24.
+//
+
+import SwiftUI
+
+@main
+struct CountryFlagsGraphQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
